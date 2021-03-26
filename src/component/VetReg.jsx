c97262dfac647054274312bb610dbe416e82e189
JoJo Myles <joneshiamyles31@gmail.com>
2021-03-26 17:24:44 -0400

    added the veterans registration form

---
+++ src/component/VetReg.jsx
@@ -0,0 +1,56 @@
+
+
+
+const VetReg = () => {
+
+
+
+
+    return (
+        
+        <form>
+            <div>
+                <label>First Name</label>
+                <input type='text' placeholder='First Name'  />
+            </div>
+            <div>
+                <label>Last Name</label>
+                <input type='text' placeholder='Last Name'  />
+            </div>
+            <div>
+                <label>Mobile Phone#</label>
+                <input type='number' placeholder=' Mobile Phone'  />
+            </div>
+            <div>
+                <label>Email</label>
+                <input type='text' placeholder='Email'  />
+            </div>
+            <div>
+                <label>City, State</label>
+                <input type='text' placeholder='City, State'  />
+            </div>
+            <div>
+                <label>Zip Code</label>
+                <input type='number' placeholder='zip code'  />
+            </div>
+            <div>
+                <label>Username:</label>
+                <input type='text' placeholder='username'  />
+            </div>
+            <div>
+                <label>Password</label>
+                <input type='password' placeholder='password'  />
+            </div>
+            <div>
+                <label>Verify Password</label>
+                <input type='text' placeholder='verify password' />
+            </div>
+            <div>
+                <input type='submit' placeholder='Submit info'  />
+            </div>
+        </form>
+        
+    )
+}
+
+export default VetReg
