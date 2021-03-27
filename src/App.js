cb8f8669eb2d6328059e7aaae9dd97b38d8cfe50
JoJo Myles <joneshiamyles31@gmail.com>
2021-03-27 19:11:15 -0400

    working on routers

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link  } from 'react-router-dom';
+import { useState } from 'react';
 import Header from './component/Header';
+import Home from './component/Home';
 import VetReg from './component/VetReg';
 
 
-function App() {
+export default function App() {
+
+
+
+
   return (
-    <>
-      <Header />
-      <VetReg />
-    </>
+
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">VetReg</Link>
+            </li>
+            <li>
+              <Link to="/">users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/VetReg">
+            <VetReg />
+          </Route>
+          <Route path="/User">
+            <User />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
+
+  function Home() {
+    return <h2>Home</h2>;
+  }
+  function VetReg() {
+    return <h2>Registration</h2>;
+  }
+  function User() {
+    return <h2>Profile</h2>;
+  }
+  
 }
 
-export default App;
