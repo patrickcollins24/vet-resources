e8cfdc102efe81be4e440df43638ebcc764bc01d
JoJo Myles <joneshiamyles31@gmail.com>
2021-03-30 16:24:51 -0400

    adding appointment form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route, Link  } from 'react-router-dom'
 import { useState } from 'react';
 import Home from './component/Home';
 import About from './component/About';
-import VetReg from './component/VetReg';
+import Appointment from './component/Appointment';
 
 
 
@@ -37,7 +37,7 @@ export default function App() {
             <About />
           </Route>
           <Route path="/VetReg">
-            <VetReg />
+            <Appointment />
           </Route>
         </Switch>
       </div>
--- src/component/Appointment.jsx
+++ src/component/Appointment.jsx
@@ -1,9 +1,41 @@
 
 
 const Appointment = () => {
+
+
     return (
         <div>
-            
+            <form>
+                <h2>Appointment</h2>
+                <br/>
+                <h3>Add Appointment</h3>
+                <div>
+                <label htmlFor='title'>
+                    Title
+                </label>
+                <input type='text' placeholder='enter appointment'/>
+                </div>
+                <label htmlFor='title'>
+                    Date
+                </label>
+                <input type='date' name='date'/>
+                <label htmlFor='title'>
+                    Time
+                </label>
+                <input type='text' name='time'/>
+                <div>
+                    <label >
+                        Reminder
+                    </label>
+                    <br/>
+                    <select>
+                    <option>text message</option>
+                    <option>email</option>
+                    </select>
+                    </div>
+                    <button className='form-btn' type='submit'>Add Appointment</button>
+                
+            </form>
         </div>
     )
 }
