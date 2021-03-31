cae4d4311d4cf54af9ac4aca5a1dd02512712aa3
JoJo Myles <joneshiamyles31@gmail.com>
2021-03-31 11:19:56 -0400

    removed files that wasn't needed

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link  } from 'react-router-dom';
-import { useState } from 'react';
-import Home from './component/Home';
-import About from './component/About';
 import Appointment from './component/Appointment';
 
 
 
 export default function App() {
-
-  const [inputInfo, setInputInfo]= useState({
-    firstName: '',
-    lastName: '',
-    mobilePhone: '',
-    city: '',
-    state: '',
-    zipCode: '',
-    username: '',
-    password: '',
-    password2: '',
-  });
-  
-
-
-
-
   return (
-
-    <Router>
       <div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/About">
-            <About />
-          </Route>
-          <Route path="/VetReg">
-            <Appointment />
-          </Route>
-        </Switch>
+        <Appointment />
       </div>
-    </Router>
-    
+
   );
-
-  
-
-  
-
-  
-  
 }
 
