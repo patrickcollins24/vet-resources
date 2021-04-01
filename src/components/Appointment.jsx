2bc94e441b45bcba9e0ad8d67edf636900323598
JoJo Myles <joneshiamyles31@gmail.com>
2021-04-01 09:03:39 -0400

    added the deleteAppointment inside the component. had the appointment list showing. searching for  the error.

--- src/components/Appointment.jsx
+++ src/components/Appointment.jsx
@@ -1,13 +1,18 @@
+import { FaTimes } from 'react-icons/fa';
 
+const Appointment = ({ appointment, onDelete }) => {
 
-const Appointment = ({appointment}) => {
+    
     return (
-        <div>
-            <h3>{appointment.title}</h3>
+        <div className='appointment'> 
+            <h3>
+                {appointment.title}{''} 
+                <FaTimes 
+                style={{color:
+                'black', cursor: 'pointer'}}
+                onClick={() =>{appointment.id}} />
             <p>{appointment.date}</p>
-            <p>{appointment.time}</p>
-            <p>{appointment.reminder}</p>
-            <p>{appointment.type}</p>
+        </h3>
         </div>
     )
 }
